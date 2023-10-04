18cff40f1a14cbef6f732644b0f5b52d1e2332fb
Jake Hu <hotranphihung@gmail.com>
2023-10-04 21:30:35 +0700

    s

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,65 +5,61 @@ import { useNavigate } from 'react-router-dom';
 import api from '../api/api';
 
 function Home() {
-  const api_url = api
+  const api_url = api;
   const [posts, setPosts] = useState([]);
+  const [page, setPage] = useState(1); // Current page
+  const [totalPages, setTotalPages] = useState(1); // Total number of pages
   const navigate = useNavigate(); // Use navigate to redirect
-  // Lấy danh sách bài viết
-  function getPosts() {
-    axios.get("http://localhost:3002/manga/all")
+
+  // Function to fetch manga items for a specific page
+  function getPosts(pageNumber) {
+    axios.get(`http://localhost:3002/manga/all/${pageNumber}`)
       .then((response) => {
         const { data } = response;
-        setPosts(data);
-        console.log(data);
+        setPosts(data.mangas);
+        setTotalPages(data.totalPages);
       })
       .catch((error) => {
         console.error(`Error fetching data: ${error}`);
       });
   }
-  const fakeData = [{
-    id: "pkfa8iLO",
-    name: "one piece",
-    number_chapter: 30,
-  },
-  {
-    id: "0xKFumO8",
-    name: "naruto",
-    number_chapter: 20
-  },
-  {
-    id: 3,
-    number_chapter: 23
-  },
-  {
-    id: 4,
-    name: "naruto",
-    number_chapter: 20
-  },
-  {
-    id: 5,
-    number_chapter: 23
-  }
-  ]
 
   useEffect(() => {
-    getPosts();
-  }, []);
+    getPosts(page);
+  }, [page]);
+
+  const handlePageChange = (newPage) => {
+    setPage(newPage);
+  };
 
   return (
     <div className='home-page'>
       <h2>Welcome to My Personal Blogs</h2>
       <p>This is the home page.</p>
       <div className='manga-list'>
-        {
-          posts.map((item, index) => (
-            <div className='manga-item' key={index} onClick={() => navigate(`/manga/${item.manga_id}`)}>
-              <div className='post-img'>
-                <img src='https://picsum.photos/200/300' alt='Ảnh bài viết' />
-              </div>
-              <p>{item.manga_name}</p>
+        {posts.map((item, index) => (
+          <div className='manga-item' key={index} onClick={() => navigate(`/manga/${item.manga_id}`)}>
+            <div className='post-img'>
+              <img src='https://picsum.photos/200/300' alt='Ảnh bài viết' />
             </div>
-          ))
-        }
+            <p>{item.manga_name}</p>
+          </div>
+        ))}
+      </div>
+      <div className="pagination">
+        <button
+          onClick={() => handlePageChange(page - 1)}
+          disabled={page === 1}
+        >
+          Previous
+        </button>
+        <span>Page {page} of {totalPages}</span>
+        <button
+          onClick={() => handlePageChange(page + 1)}
+          disabled={page === totalPages}
+        >
+          Next
+        </button>
       </div>
     </div>
   );
