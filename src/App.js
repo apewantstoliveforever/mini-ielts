af372b917eb0ffb22f2c72735a4aeea271cc8b7c
Jake Hu <hotranphihung@gmail.com>
2023-11-05 21:25:03 +0700

    responsive

--- src/App.js
+++ src/App.js
@@ -5,9 +5,6 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Layout/Header';
 import Footer from './components/Layout/Footer';
 import Home from './pages/Home';
-import CreatePost from './pages/CreatePost';
-import Manga from './pages/Manga';
-import Chapter from './components/Chapter/Chapter';
 import Listen from './pages/Listen';
 import Read from './pages/Read';
 import NewIelts from './pages/NewIelts';
@@ -23,10 +20,6 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" exact element={<Home />} />
-          <Route path="/create-post" element={<CreatePost />} />
-          <Route path="/manga/:id" element={<Manga />} />
-          <Route path="/manga" element={<Manga />} />
-          <Route path="/manga/:id/:chapter" element={<Chapter />} />
           <Route path="/read" element={<Read />} />
           <Route path='/new-ielts' element={<NewIelts />} />
           <Route path='/create' element={<Create />} />
