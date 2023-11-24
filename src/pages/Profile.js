1128829041d7e1b007a8dd0be0039cc81191ea9f
Jake Hu <hotranphihung@gmail.com>
2023-11-24 20:41:17 +0700

    s

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -7,6 +7,7 @@ import { useSelector } from "react-redux";
 
 const Profile = () => {
     const { user: currentUser } = useSelector((state) => state.auth);
+    console.log(currentUser);
 
     if (!currentUser) {
         return <Navigate to="/login" />;
@@ -17,6 +18,14 @@ const Profile = () => {
             <h3>
                 <strong>{currentUser.email}</strong> Profile
             </h3>
+            <p>
+                Profile image
+                <img
+                    src={currentUser.photoURL}
+                    alt="profile"
+                    style={{ width: "100px" }}
+                />
+            </p>
             <p>
                 <strong>Token:</strong> {currentUser.token.substring(0, 20)} ...{" "}
                 {currentUser.token.substr(currentUser.token.length - 20)}
