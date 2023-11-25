be4e06c293a9e9a518d1b6b9aec7c2e2d918fdf6
Jake Hu <hotranphihung@gmail.com>
2023-11-25 21:02:47 +0700

    s

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,5 @@
 //const api = 'https://api-blog.apewannaliveforever.online';
 //const api = 'http://localhost:3002';
 // api = 'https://api-blog.apewannaliveforever.online'; or api = 'http://localhost:3002';
-const api ='http://localhost:3002';
+const api ='https://api-blog.apewannaliveforever.online'
 export default api;
